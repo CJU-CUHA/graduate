a5e70469f2b12b20b989f1bb444729d131bf3328
king_ing_e <2022011870@cju.ac.kr>
2025-02-22 23:22:25 +0900

    Graduate Project Start real

--- BE/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-
-        EvtxReader evtxReader = new EvtxReader("Security.evtx");
-        evtxReader.parseEvtxFile(0);
-    }
-}

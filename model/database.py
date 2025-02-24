2a736dbc0e84de54fd8425d7816053b6bf2d2598
king_ing_e <2022011870@cju.ac.kr>
2025-02-24 15:59:09 +0900

    찐으로 시작

--- model/database.py
+++ model/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 import databases
-
+import json
+with open("model\\key.json", 'r') as f:
+    db_password = json.load(f)
 # MySQL 연결 정보 (본인 설정에 맞게 변경)
-DATABASE_URL = "mysql+aiomysql://root:rc583013@localhost:3306/graduate_security"
-
+DATABASE_URL = f"mysql+aiomysql://root:{db_password['db_password']}@localhost:3306/graduate_security"
 # 비동기 엔진 생성
 engine = create_async_engine(DATABASE_URL, echo=True)
 
